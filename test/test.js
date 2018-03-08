55e9086a03636a62d98703e7f413c64157e3a17f
Sylvain Leroux <sylvain@chicoree.fr>
2018-03-08 15:22:58 +0100

    Cache values between calls

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 const debug = require("debug")("mmoize:tests");
 
 const assert = require('chai').assert;
+const id = (x) => x;
 
 /* temporary patch for https://github.com/chaijs/chai/issues/1116 */
 assert.fail = require('assert').fail;
@@ -14,5 +15,37 @@ describe("module", function() {
 
     it("should export as a function", function() {
         assert.typeOf(mmoize, 'function');
+    });
+});
+
+describe("mmoize", function() {
+    const mmoize = require("../index.js");
+
+    it("should return a promise-like object", function() {
+        const f = mmoize(id);
+        assert.typeOf(f, 'function');
+
+        const p = f("a");
+        assert.typeOf(p, 'object');
+        assert.typeOf(p.then, 'function');
+        assert.typeOf(p.catch, 'function');
+    });
+
+    it("should invoke the contructor for missing values", function() {
+        let n = 0;
+        const f = mmoize((key) => n += 1);
+        return f("a").then((v) => {
+            assert.equal(v, 1);
+            assert.equal(n, 1);
+        });
+    });
+
+    it("should NOT invoke the contructor for cached values", function() {
+        let n = 0;
+        const f = mmoize((key) => n += 1);
+        return f("a").then(() => f("a")).then((v) => {
+            assert.equal(v, 1);
+            assert.equal(n, 1);
+        });
     });
 });
